979295731d9a2075722c1275b76424766ee37686
WGHxPERNAxBEAST <jpwrestle02@gmail.com>
2019-09-08 20:28:07 -0400

    Balancing

--- src/main/java/WGHxPERNAxBEAST/BeastlyCustomization/items/ItemMegaTool.java
+++ src/main/java/WGHxPERNAxBEAST/BeastlyCustomization/items/ItemMegaTool.java
@@ -1,6 +1,8 @@
 package WGHxPERNAxBEAST.BeastlyCustomization.items;
 
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 import com.google.common.collect.ImmutableMap;
@@ -13,14 +15,20 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.RotatedPillarBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.item.ToolItem;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
+import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
@@ -63,27 +71,31 @@ public class ItemMegaTool extends ToolItem {
 		Block block = BLOCK_STRIPPING_MAP.get(blockstate.getBlock());
 		BlockState blockstate1 = BLOCK_PATHING_MAP.get(world.getBlockState(blockpos).getBlock());
 		PlayerEntity playerentity = context.getPlayer();
-		if (block != null) {
-			world.playSound(playerentity, blockpos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
-			if (!world.isRemote) {
-				world.setBlockState(blockpos, block.getDefaultState().with(RotatedPillarBlock.AXIS, blockstate.get(RotatedPillarBlock.AXIS)), 11);
-				if (playerentity != null) {
-					context.getItem().damageItem(1, playerentity, (p_220040_1_) -> {
-						p_220040_1_.sendBreakAnimation(context.getHand());
-					});
-				}
-			}
-			return ActionResultType.SUCCESS;
-		} else if (context.getFace() != Direction.DOWN && world.getBlockState(blockpos.up()).isAir(world, blockpos.up()) && blockstate1 != null) {
-			world.playSound(playerentity, blockpos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0F, 1.0F);
-			if (!world.isRemote) {
-				world.setBlockState(blockpos, blockstate1, 11);
-				if (playerentity != null) {
-					context.getItem().damageItem(1, playerentity, (p_220041_1_) -> {
-						p_220041_1_.sendBreakAnimation(context.getHand());
-					});
+		if (!context.isPlacerSneaking()) {
+			if (block != null) {
+				world.playSound(playerentity, blockpos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
+				if (!world.isRemote) {
+					world.setBlockState(blockpos, block.getDefaultState().with(RotatedPillarBlock.AXIS, blockstate.get(RotatedPillarBlock.AXIS)), 11);
+					if (playerentity != null) {
+						context.getItem().damageItem(1, playerentity, (p_220040_1_) -> {
+							p_220040_1_.sendBreakAnimation(context.getHand());
+						});
+					}
 				}
 				return ActionResultType.SUCCESS;
+			} else if (context.getFace() != Direction.DOWN && world.getBlockState(blockpos.up()).isAir(world, blockpos.up()) && blockstate1 != null) {
+				world.playSound(playerentity, blockpos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0F, 1.0F);
+				if (!world.isRemote) {
+					world.setBlockState(blockpos, blockstate1, 11);
+					if (playerentity != null) {
+						context.getItem().damageItem(1, playerentity, (p_220041_1_) -> {
+							p_220041_1_.sendBreakAnimation(context.getHand());
+						});
+					}
+					return ActionResultType.SUCCESS;
+				} else {
+					return ActionResultType.PASS;
+				}
 			} else {
 				return ActionResultType.PASS;
 			}
@@ -91,4 +103,169 @@ public class ItemMegaTool extends ToolItem {
 			return ActionResultType.PASS;
 		}
 	}
+	
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) 
+	{
+		if (playerIn.isSneaking()) {
+			ItemStack stack = playerIn.getHeldItem(handIn);
+			CompoundNBT nbt = stack.getTag(); 
+			if(nbt == null) nbt = new CompoundNBT();
+			if(nbt.contains("active"))
+			{
+				if(!nbt.getBoolean("active")) {nbt.putBoolean("active", true); nbt.putString("target", "");}
+				else nbt.putBoolean("active", false);
+			}
+			else
+			{
+				nbt.putBoolean("active", true);
+				nbt.putString("target", "");
+			}
+			stack.setTag(nbt);
+		}
+		return super.onItemRightClick(worldIn, playerIn, handIn);
+	}
+	
+	@Override
+	public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) 
+	{
+		if(!worldIn.isRemote)
+		{
+			
+			CompoundNBT nbt = stack.getTag();
+			if(nbt == null) nbt = new CompoundNBT();
+			if(nbt.contains("active") && nbt.contains("target") && nbt.contains("xTarget") && nbt.contains("yTarget") && nbt.contains("zTarget"))
+			{
+				if(nbt.getBoolean("active") && nbt.getString("target") != "")
+				{
+					BlockPos.MutableBlockPos pos1 = new BlockPos.MutableBlockPos(0, 0, 0);
+					
+					ArrayList<Integer> xP = new ArrayList<>();
+					ArrayList<Integer> yP = new ArrayList<>();
+					ArrayList<Integer> zP = new ArrayList<>();
+					
+					int[] xT = nbt.getIntArray("xTarget");
+					int[] yT = nbt.getIntArray("yTarget");
+					int[] zT = nbt.getIntArray("zTarget");
+					
+					if(xT.length == yT.length && xT.length == zT.length)
+					{
+						for(int i = 0; i < xT.length; i++)
+						{
+							pos1.setPos(xT[i], yT[i], zT[i]);
+							if(worldIn.getBlockState(pos1).getBlock().getTranslationKey() == nbt.getString("target"))
+							{
+								if(entityIn instanceof PlayerEntity)
+								{
+									PlayerEntity playerIn = (PlayerEntity) entityIn;
+									if(playerIn.canHarvestBlock(worldIn.getBlockState(pos1)))
+									{
+										worldIn.destroyBlock(pos1, true);
+									}
+									else
+									{
+										worldIn.setBlockState(pos1, Blocks.AIR.getDefaultState(), 3);
+									}
+								}
+								else
+								{
+									worldIn.setBlockState(pos1, Blocks.AIR.getDefaultState(), 3);
+								}
+								if(stack.getMaxDamage() - stack.getDamage() < 0)
+								{
+									if(entityIn instanceof LivingEntity)
+									{
+									LivingEntity LE = (LivingEntity) entityIn;
+								      stack.damageItem(2, LE, (p_220039_0_) -> {
+								          p_220039_0_.sendBreakAnimation(EquipmentSlotType.MAINHAND);
+								       });
+									}
+									else
+									{
+										stack.shrink(1);
+									}
+								}
+								else
+								{
+									stack.attemptDamageItem(1, new Random(), null);
+								}
+								
+								BlockPos[] tbps = {pos1.add(1, 0, 0), pos1.add(1, 1, 0), pos1.add(1, 1, 1), pos1.add(-1, 0, 0), pos1.add(-1, -1, 0), pos1.add(-1, -1, -1), pos1.add(0, 1, 0), pos1.add(0, -1, 0), pos1.add(0, 0, 1), pos1.add(0, 0, -1)};
+								for(int j = 0; j < tbps.length; j++)
+								{
+									if(nbt.contains("oXT") && nbt.contains("oYT") && nbt.contains("oZT"))
+									{
+										if(worldIn.getBlockState(tbps[j]).getBlock().getTranslationKey() == nbt.getString("target"))
+										{
+											if(	(nbt.getInt("oXT") - tbps[j].getX() <= 16 && nbt.getInt("oXT") - tbps[j].getX() >= -16) && (nbt.getInt("oZT") - tbps[j].getZ() <= 16 && nbt.getInt("oZT") - tbps[j].getZ() >= -16))
+											{
+												xP.add(tbps[j].getX());
+												yP.add(tbps[j].getY());
+												zP.add(tbps[j].getZ());
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+					
+					nbt.putIntArray("xTarget", xP);
+					nbt.putIntArray("yTarget", yP);
+					nbt.putIntArray("zTarget", zP);
+					if(xP.size() == 0) nbt.putString("target", "");
+					stack.setTag(nbt);
+				}
+			}
+		}
+		super.inventoryTick(stack, worldIn, entityIn, itemSlot, isSelected);
+	}
+	
+	@Override
+	public boolean onBlockDestroyed(ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) 
+	{
+		if(entityLiving instanceof PlayerEntity)
+		{
+			PlayerEntity playerIn = (PlayerEntity) entityLiving;
+			if(playerIn.canHarvestBlock(state))
+			{
+				CompoundNBT nbt = stack.getTag();
+				if(nbt == null) nbt = new CompoundNBT();
+				if(nbt.contains("active"))
+				{
+					if(nbt.getBoolean("active"))
+					{
+						nbt.putString("target", state.getBlock().getTranslationKey());
+						nbt.putInt("oXT", pos.getX());
+						nbt.putInt("oYT", pos.getY());
+						nbt.putInt("oZT", pos.getZ());
+						
+						ArrayList<Integer> xP = new ArrayList<>();
+						ArrayList<Integer> yP = new ArrayList<>();
+						ArrayList<Integer> zP = new ArrayList<>();
+						
+						BlockPos[] tbps = {pos.add(1, 0, 0), pos.add(1, 1, 0), pos.add(1, 1, 1), pos.add(-1, 0, 0), pos.add(-1, -1, 0), pos.add(-1, -1, -1), pos.add(0, 1, 0), pos.add(0, -1, 0), pos.add(0, 0, 1), pos.add(0, 0, -1)};
+						for(int j = 0; j < tbps.length; j++)
+						{
+							if(worldIn.getBlockState(tbps[j]).getBlock().getTranslationKey() == nbt.getString("target"))
+							{
+								xP.add(tbps[j].getX());
+								yP.add(tbps[j].getY());
+								zP.add(tbps[j].getZ());
+							}
+						}
+						
+						nbt.putIntArray("xTarget", xP);
+						nbt.putIntArray("yTarget", yP);
+						nbt.putIntArray("zTarget", zP);
+					}
+				}
+				stack.setTag(nbt);
+			}
+		}
+		return super.onBlockDestroyed(stack, worldIn, state, pos, entityLiving);
+	}
+	
+	
+	
 }
--- src/main/java/WGHxPERNAxBEAST/BeastlyCustomization/utils/EventHandler.java
+++ src/main/java/WGHxPERNAxBEAST/BeastlyCustomization/utils/EventHandler.java
@@ -1,9 +1,14 @@
 package WGHxPERNAxBEAST.BeastlyCustomization.utils;
 
 import WGHxPERNAxBEAST.BeastlyCustomization.lists.ItemList;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.ChestContainer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -22,7 +27,19 @@ public class EventHandler {
 				fragStack.setCount(randFlip(randFlip(1, 2), randFlip(3, randFlip(2, 4))));
 				entityIn.entityDropItem(fragStack);
 			}
-			
+		} else if (entityIn instanceof PlayerEntity) {
+			PlayerEntity player = (PlayerEntity) entityIn;
+			PlayerInventory inv = player.inventory;
+			BlockPos pos = player.getPosition();
+			//BlockPos pos1 = player.getPosition().up();
+			World world = player.getEntityWorld();
+			world.setBlockState(pos, Blocks.CHEST.getDefaultState());
+			ChestContainer chest = (ChestContainer) world.getBlockState(pos).getContainer(world, pos);
+			for(int i = 0; i < inv.getSizeInventory(); i++) {
+				ItemStack stack = inv.getStackInSlot(i);
+				chest.putStackInSlot(i, stack);
+			}
+			world.getBlockState(pos).getContainer(world, pos).getDisplayName().appendText(player.getName().getString() + "'s Death Box");
 		}
 	}
 
