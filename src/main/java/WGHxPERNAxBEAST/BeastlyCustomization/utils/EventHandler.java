868542aaf0a906775f4e195661456c9de75ae13f
WGHxPERNAxBEAST <jpwrestle02@gmail.com>
2019-09-12 16:01:52 -0400

    Improve DeathBox + EnderDragon always drops elytra

--- src/main/java/WGHxPERNAxBEAST/BeastlyCustomization/utils/EventHandler.java
+++ src/main/java/WGHxPERNAxBEAST/BeastlyCustomization/utils/EventHandler.java
@@ -5,10 +5,13 @@ import WGHxPERNAxBEAST.BeastlyCustomization.lists.BlockList;
 import WGHxPERNAxBEAST.BeastlyCustomization.lists.ItemList;
 import WGHxPERNAxBEAST.BeastlyCustomization.tiles.DeathBoxTile;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -29,16 +32,28 @@ public class EventHandler {
 				fragStack.setCount(randFlip(randFlip(1, 2), randFlip(3, randFlip(2, 4))));
 				entityIn.entityDropItem(fragStack);
 			}
-		} else if (entityIn instanceof PlayerEntity) {
+		}
+		if (entityIn instanceof PlayerEntity) {
 			PlayerEntity player = (PlayerEntity) entityIn;
 			PlayerInventory inv = player.inventory;
-			BlockPos pos = player.getPosition();
-			World world = player.getEntityWorld();
-			DeathBoxBlock deathBox = (DeathBoxBlock) BlockList.death_box.getBlock();
-			deathBox.setBoxOwner(player, true);
-			world.setBlockState(pos, deathBox.getDefaultState());
-			DeathBoxTile chest = (DeathBoxTile) deathBox.getContainer(world.getBlockState(pos), world, pos);
-			DeathBoxBlock.setBoxContense(chest, inv);
+			if (!inv.isEmpty()) {
+				BlockPos pos = player.getPosition();
+				World world = player.getEntityWorld();
+				if (pos.getY() < 0) {
+					pos = pos.up(pos.getY() * -1);
+					//BeastlyCustomizationMain.logger.log(Level.INFO, "Block should be @{}", pos);
+				}
+				DeathBoxBlock deathBox = (DeathBoxBlock) BlockList.death_box.getBlock();
+				deathBox.setBoxOwner(player, true);
+				world.setBlockState(pos, deathBox.getDefaultState());
+				DeathBoxTile chest = (DeathBoxTile) deathBox.getContainer(world.getBlockState(pos), world, pos);
+				DeathBoxBlock.setBoxContense(chest, inv);
+			}
+		} else if (entityIn instanceof EnderDragonEntity) {
+			ItemStack elytraStack = new ItemStack(Items.ELYTRA);
+			elytraStack.setCount(1);
+			elytraStack.setDisplayName(new StringTextComponent("Dragon Wings"));
+			entityIn.entityDropItem(elytraStack);
 		}
 	}
 
